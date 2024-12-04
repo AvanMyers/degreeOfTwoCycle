937ff804190aa1a966274b4435096d69233ae436
AvanMyers <smail1341@gmail.com>
2024-12-04 11:41:14 +0300

    re

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeOFTwoCycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int starterNumber = 2;
+            int finalNumber = starterNumber;
+            int degree = 1;
+            int minRandom = 2;
+            int maxRandom = 100;
+            int value = random.Next(minRandom, maxRandom);
+            bool isOpen = true;
+
+            Console.WriteLine("Случайное число " + value);
+
+            while (value <= finalNumber)
+            {
+                finalNumber *= starterNumber;
+                degree++;
+            }
+
+            Console.WriteLine($"Для числа {value} будет {starterNumber} в степени {degree}, то есть {finalNumber}. {value} < {finalNumber}");
+            Console.ReadKey();
+        }
+    }
+}
